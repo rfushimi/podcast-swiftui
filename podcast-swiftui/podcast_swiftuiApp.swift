531f6d8cc1c6d53ed8a294efb8270df0492ff0ad
Ryohei Fushimi <rfushimi@users.noreply.github.com>
2022-02-26 16:37:33 +0900

    Initial Commit

---
+++ podcast-swiftui/podcast_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  podcast_swiftuiApp.swift
+//  podcast-swiftui
+//
+//  Created by Ryohei Fushimi on 2022/02/26.
+//
+
+import SwiftUI
+
+@main
+struct podcast_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
